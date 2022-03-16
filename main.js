6787d6277d3af60f2c43d1ba166cb44927f71e1d
grayTurtle01 <the.correo@protonmail.com>
2022-03-16 10:56:57 -0600

    update: refactor functions

--- main.js
+++ main.js
@@ -22,53 +22,20 @@ function get_pokemon(name){
 
 
           /*** Image ***/  
-          sprites = data['sprites']
-          sprite = sprites['front_default']
-
-          anime_image = sprites['other']['dream_world']['front_default']
-          document.querySelector("img").src = sprite
-          //~ document.querySelector("img").src = anime_image
+          setImage(data)  
 
           /*** Name ***/    
-          number = data['id']
-          name = data['name']
-          title = `#${number} ${name}`  
-
-          document.querySelector("#poke-name").innerText = capitalize(name)  
-          document.querySelector("#number").innerText = number  
+          setNameAndNumber(data)
 
           /*** Height and Weight ***/
-          document.querySelector("#height").innerText = data['height']
-          console.log(data['weigth']  )    
-          document.querySelector("#weight").innerText = data['weight']    
+          setHeightAndWeight(data)
 
           /*** Types ***/    
-          types = []
-
-          for( item of data['types']){
-                type = item['type']['name']
-                types.push(type)
-          }  
-          
-          document.querySelector("#types").innerText =  types 
+          setTypes(data)
 
           /*** Stats ***/
-          console.log(data['stats'])
-          stats = data['stats']
-
-          hp = stats[0]['base_stat']
-          attack = stats[1]['base_stat']
-          defense = stats[2]['base_stat']
-          special_attack = stats[3]['base_stat']
-          special_defense = stats[4]['base_stat']
-          speed = stats[5]['base_stat']
-
-          document.querySelector("#hp").innerText = hp
-          document.querySelector("#attack").innerText = attack
-          document.querySelector("#defense").innerText = defense            
-          document.querySelector("#special-attack").innerText = special_attack
-          document.querySelector("#special-defense").innerText = special_defense
-          document.querySelector("#speed").innerText = speed  
+          setStats(data)
+         
           
        })
        .catch( err => {
@@ -82,6 +49,58 @@ btn.onclick = function(){
     name = name.toLowerCase()
     get_pokemon(name)
     
+}
+
+function setImage(data){
+    sprites = data['sprites']
+    sprite = sprites['front_default']
+
+    anime_image = sprites['other']['dream_world']['front_default']
+    document.querySelector("img").src = sprite
+    //~ document.querySelector("img").src = anime_image
+}
+
+function setNameAndNumber(data){
+    number = data['id']
+    name = data['name']
+    title = `#${number} ${name}`  
+
+    document.querySelector("#poke-name").innerText = capitalize(name)  
+    document.querySelector("#number").innerText = number  
+}
+
+function setHeightAndWeight(data){
+    document.querySelector("#height").innerText = data['height']
+    document.querySelector("#weight").innerText = data['weight']    
+}
+
+function setTypes(data){
+    types = []
+
+    for( item of data['types']){
+        type = item['type']['name']
+        types.push(type)
+    }  
+
+    document.querySelector("#types").innerText =  types 
+}
+
+function setStats(data){
+    stats = data['stats']
+
+    hp = stats[0]['base_stat']
+    attack = stats[1]['base_stat']
+    defense = stats[2]['base_stat']
+    special_attack = stats[3]['base_stat']
+    special_defense = stats[4]['base_stat']
+    speed = stats[5]['base_stat']
+
+    document.querySelector("#hp").innerText = hp
+    document.querySelector("#attack").innerText = attack
+    document.querySelector("#defense").innerText = defense            
+    document.querySelector("#special-attack").innerText = special_attack
+    document.querySelector("#special-defense").innerText = special_defense
+    document.querySelector("#speed").innerText = speed  
 }
 
 function capitalize(text){
